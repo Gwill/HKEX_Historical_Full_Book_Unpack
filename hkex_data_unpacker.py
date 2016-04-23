d9c4a6627dc99ba0588192b2164132cc517babab
G_will <g_will@ieqi.com>
2016-04-23 22:11:18 +0800

    update

--- hkex_data_unpacker.py
+++ hkex_data_unpacker.py
@@ -7,44 +7,52 @@ class Unpacker(object):
 
     def __init__(self, packed_data_file_path):
 
+        # 载入数据
         with open(packed_data_file_path, 'rb') as f:
             self.data = f.read()
 
         self.data_len = len(self.data)
-
-        print('self.data_len', self.data_len)
-
+        
         self.offset = 0
+        self.result = []
 
 
-    def _unpack_RecLen(self, d):
-        assert len(d) == 2
+    def _unpack_RecLen(self):
+        field_len = 2
+
+        d = self.data[self.offset:self.offset+2]
+        self.offset += 2
+
+        assert len(d) == field_len
         RecLen = struct.unpack('H', d)[0]
+
         return RecLen
 
+    def _unpack_RecLen
 
-    def _rec_count(self):
-        count = 0
-        offset = 0
 
-        while self.data_len > 0:
-            current_RecLen = self._unpack_RecLen(self.data[offset:offset+2])
+    # def _rec_count(self):
+    #     count = 0
+    #     offset = 0
 
-            print('current_RecLen', current_RecLen)
+    #     while self.data_len > 0:
+    #         current_RecLen = self._unpack_RecLen(self.data[offset:offset+2])
 
-            self.data_len = self.data_len - current_RecLen
+    #         print('current_RecLen', current_RecLen)
 
-            print('self.data_len', self.data_len)
+    #         self.data_len = self.data_len - current_RecLen
 
-            offset = offset + current_RecLen
+    #         print('self.data_len', self.data_len)
 
-            print('offset', offset)
+    #         offset = offset + current_RecLen
 
-            count = count + 1
-            print('count', count)
-            #break
+    #         print('offset', offset)
 
-        return count
+    #         count = count + 1
+    #         print('count', count)
+    #         #break
+
+    #     return count
 
 
     def test(self):
