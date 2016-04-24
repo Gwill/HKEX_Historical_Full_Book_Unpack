f38f04e5d8d2dd4c3e4c19220e8e8bf3caae3079
G_will <g_will@ieqi.com>
2016-04-24 20:57:59 +0800

    update

--- hkex_data_unpacker.py
+++ hkex_data_unpacker.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import struct
+from datetime import datetime
 
 
 class Unpacker(object):
@@ -21,7 +22,6 @@ class Unpacker(object):
         self.SecurityDefinition_results = []
         self.LiquidityProvider_results = []
         self.CurrentRate_results = []
-
 
     def _unpack_RecordLength_RecLen(self):
         field_len = 2
@@ -129,29 +129,6 @@ class Unpacker(object):
             return False
         return True
 
-    # def _rec_count(self):
-    #     count = 0
-    #     offset = 0
-
-    #     while self.data_len > 0:
-    #         current_RecLen = self._unpack_RecLen(self.data[offset:offset+2])
-
-    #         print('current_RecLen', current_RecLen)
-
-    #         self.data_len = self.data_len - current_RecLen
-
-    #         print('self.data_len', self.data_len)
-
-    #         offset = offset + current_RecLen
-
-    #         print('offset', offset)
-
-    #         count = count + 1
-    #         print('count', count)
-    #         #break
-
-    #     return count
-
     def _parse_msg_data(self, MsgType, MsgData):
         return
 
@@ -159,19 +136,90 @@ class Unpacker(object):
         return ''.join(['{:02X}'.format(ord(d)) for d in data])
 
 
+    def _parse_MarketDefinition(self, data, result):
 
-    def test(self):
-        # offset = 6144
-        # print(self._unpack_RecLen(self.data[offset:offset+2]))
-        # print(self._rec_count())
+        result['MarketCode'] = struct.unpack('4s', data[0:4])[0].strip()
+        result['MarketName'] = struct.unpack('25s', data[4:4+25])[0].strip()
+        result['CurrencyCode'] = struct.unpack('3s', data[29:29+3])[0].strip()
+        result['NumberOfSecurities'] = struct.unpack('I', data[32:32+4])[0]
 
-        counter = 30
+        return result
 
-        while counter:
+    def _parse_SecurityDefinition(self, data, result):
+        data_len = len(data)
+
+        spec = [
+            ('SecurityCode', 'I', 4),
+            ('MarketCode', '4s', 4),
+            ('ISINCode', '12s', 12),
+            ('InstrumentType', '4s', 4),
+            ('SpreadTableCode', '2s', 2),
+            ('SecurityShortName', '40s', 40),
+            ('CurrencyCode', '3s', 3),
+            ('SecurityNameGCCS', '60s', 60),
+            ('SecurityNameGB', '60s', 60),
+            ('LotSize', 'I', 4),
+            ('PreviousClosingPrice', 'i', 4),
+            ('Filler', '1s', 1),
+            ('ShortSellFlag', '1s', 1),
+            ('Filler', '1s', 1),
+            ('CCASSFlag', '1s', 1),
+            ('DummySecurityFlag', '1s', 1),
+            ('TestSecurityFlag', '1s', 1),
+            ('StampDutyFlag', '1s', 1),
+            ('Filler', '1s', 1),
+            ('ListingDate', 'I', 4),
+            ('DelistingDate', 'I', 4),
+            ('FreeText', '38s', 38),
+            ('EFNFlag', '1s', 1),
+            ('AccruedInterest', 'I', 4),
+            ('CouponRate', 'I', 4),
+            ('ConversionRatio', 'I', 4),
+            ('StrikePrice', 'i', 4),
+            ('MaturityDate', 'I', 4),
+            ('CallPutFlag', '1s', 1),
+            ('Style', '1s', 1),
+            ('NoUnderlyingSecurities', 'H', 2),
+            # ('UnderlyingSecurityCode', 'I', 4),
+            # ('UnderlyingSecurityWeight', 'I', 4),
+        ]
+
+        offset = 0
+
+        for s in spec:
+            result[s[0]] = struct.unpack(s[1], data[offset:offset+s[2]])[0]
+
+            if s[0] == 'SecurityNameGCCS':
+                result['SecurityNameGCCS'] = result['SecurityNameGCCS'].decode('utf-16').encode('utf-8')
+                # print('SecurityNameGCCS: {}'.format(result['SecurityNameGCCS']))
+
+            if s[0] == 'SecurityNameGB':
+                result['SecurityNameGB'] = result['SecurityNameGB'].decode('utf-16').encode('utf-8')
+                # print('SecurityNameGB: {}'.format(result['SecurityNameGB']))
+
+            if isinstance(result[s[0]], str):
+                result[s[0]] = result[s[0]].strip()
+
+            offset += s[2]
+
+        return result
+
+    def _parse_LiquidityProvider(self, data, result):
+        pass
+
+    def _parse_CurrentRate(self, data, result):
+        pass
+
+
+    def unpack(self):
+
+        TotalMsg = 0
+
+        while True:
 
             RecLen = self._unpack_RecordLength_RecLen()
 
-            print('RecLen: {}'.format(RecLen))
+            # print('RecLen: {}'.format(RecLen))
 
             PktSize = self._unpack_PacketHeader_PktSize()
             MsgCount = self._unpack_PacketHeader_MsgCount()
@@ -179,9 +227,9 @@ class Unpacker(object):
             SeqNum = self._unpack_PacketHeader_SeqNum()
             SendTime = self._unpack_PacketHeader_SendTime()
 
-            print('PktSize: {}, MsgCount: {}, Filler: {}, SeqNum: {}, SendTime: {}'.format(
-                PktSize, MsgCount, self._data_hex(Filler), SeqNum, SendTime
-            ))
+            # print('PktSize: {}, MsgCount: {}, Filler: {}, SeqNum: {}, SendTime: {}'.format(
+            #     PktSize, MsgCount, self._data_hex(Filler), SeqNum, SendTime
+            # ))
 
             while MsgCount:
 
@@ -192,22 +240,40 @@ class Unpacker(object):
 
                 MsgCount -= 1
 
-                if MsgType != 100 and MsgType != 11:
+                TotalMsg += 1
 
+
+                result = {}
+                result['MsgType'] = MsgType
+                result['SendTime'] = datetime.fromtimestamp(SendTime/1000000000.0).strftime('%Y-%m-%d %H:%M:%S.%f')
+
+                # Market Definition
+                if MsgType == 10:
+                    result = self._parse_MarketDefinition(MsgData, result)
+                    self.MarketDefinition_results.append(result)
+
+                # Security Definition
+                if MsgType == 11:
+                    result = self._parse_SecurityDefinition(MsgData, result)
+                    self.SecurityDefinition_results.append(result)
+                    print(result)
                     print('MsgSize: {}, MsgType: {}, MsgData: {}'.format(MsgSize, MsgType, self._data_hex(MsgData)))
+
+
+                # if MsgType != 100 and MsgType != 11:
+
+                #     
 
 
             if not self._check_valid_offset():
                 break
 
-
-            # counter -= 1
-            # pass
-
+        print('TotalMsg: {}'.format(TotalMsg))
+        print('Total MarketDefinition Results: {}'.format(len(self.MarketDefinition_results)))
 
 
 if __name__ == '__main__':
     data_file = 'MC01_All_20130904'
 
     unpacker = Unpacker(data_file)
-    unpacker.test()
+    unpacker.unpack()
